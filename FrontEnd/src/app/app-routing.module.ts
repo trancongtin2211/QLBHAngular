2c0c4bd7bb3dccfef20517e1cbfe1679127d16a7
DESKTOP-S1OO6RK\Asus TUF <tintran2211@gmail.com>
2022-03-09 22:47:05 +0700

    adjust frontend

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+import { RoleComponent } from './role/role.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'restaurant', component:RestaurantComponent},
+  {path : 'role', component:RoleComponent},
+  {path : '', component:HomeComponent},
+  {path : '**', component:PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,117 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+    constructor(private route:ActivatedRoute,private router:Router){}
   title = 'FrontEnd';
+  items: MenuItem[] = [];
+
+    ngOnInit() {
+        
+        this.items = [
+            {
+                label:'Nhà Hàng',
+                icon:'pi pi-fw pi-home',
+                command:() =>this.router.navigate(['/restaurant']),
+            },
+            {
+                label:'Quyền',
+                icon:'pi pi-fw pi-user-edit',
+                command:() =>this.router.navigate(['/role']),
+            },
+            {
+                label:'Tình Trạng',
+                icon:'pi pi-fw pi-flag',
+            },
+            {
+                label:'Khu Vực',
+                icon:'pi pi-fw pi-share-alt',
+            },
+            {
+                label:'Đơn vị',
+                icon:'pi pi-fw pi-share-alt',
+                items:[
+                    {
+                        label:'New',
+                        icon:'pi pi-fw pi-plus',
+                        items:[
+                        {
+                            label:'Bookmark',
+                            icon:'pi pi-fw pi-bookmark'
+                        },
+                        {
+                            label:'Video',
+                            icon:'pi pi-fw pi-video'
+                        },
+
+                        ]
+                    },
+                    {
+                        label:'Delete',
+                        icon:'pi pi-fw pi-trash'
+                    },
+                    {
+                        separator:true
+                    },
+                    {
+                        label:'Export',
+                        icon:'pi pi-fw pi-external-link'
+                    }
+                ]
+            },
+            {
+                label:'Thể loại',
+                icon:'pi pi-fw pi-list'
+            },
+            {
+                label:'Nhân sự',
+                icon:'pi pi-fw pi-user'
+            },
+            {
+                label:'Bàn ăn',
+                icon:'pi pi-fw pi-table'
+            },
+            {
+                label:'Bàn ăn',
+                icon:'pi pi-fw pi-table'
+            },
+            {
+                label:'Thức ăn',
+                icon:'pi pi-fw pi-calendar',
+                items:[
+                    {
+                        label:'New',
+                        icon:'pi pi-fw pi-plus',
+                        items:[
+                        {
+                            label:'Bookmark',
+                            icon:'pi pi-fw pi-bookmark'
+                        },
+                        {
+                            label:'Video',
+                            icon:'pi pi-fw pi-video'
+                        },
+
+                        ]
+                    },
+                    {
+                        label:'Delete',
+                        icon:'pi pi-fw pi-trash'
+                    },
+                    {
+                        separator:true
+                    },
+                    {
+                        label:'Export',
+                        icon:'pi pi-fw pi-external-link'
+                    }
+                ]
+            },
+            {
+                label:'Logout',
+                icon:'pi pi-fw pi-power-off'
+            }
+            
+        ];
+    }
 }
